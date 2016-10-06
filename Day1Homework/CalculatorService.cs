2756bc761431cb5c29ca802a5d0bf9dc3685a8ff
salyfish <saltyfish0417@gmail.com>
2016-10-07 01:07:11 +0800

    Day1Homework

---
+++ Day1Homework/CalculatorService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+public class Calculator
+{
+    public ArrayList GetSumByGroup<T>(IEnumerable<T> elements, string column, int group)
+    {
+        ArrayList result_sum = new ArrayList(); //儲存各組數值加總結果
+
+        for (int i = 0; i < elements.Count(); i += group)
+        {
+            result_sum.Add(elements.Skip(i).Take(group).Sum(x => Convert.ToInt32(x.GetType().GetProperty(column).GetValue(x))));
+        }
+
+        return result_sum;
+    }
+}
+
